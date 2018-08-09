8089dc4c9e417babff64c00890f40a83bca86b2c
Yisake <yisaketsega@gmail.com>
2018-08-09 04:00:21 -0400

    show add

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,12 +7,16 @@ end
     @article=Article.new(article_params)
     if @article.save
       flash[:notice]= "Article sucessfully created"
-      redirect_to articles_show(@article)
+      redirect_to  article_path(@article)
     else
-      render 'new '
+      render 'new'
     end
   end 
   
+  
+  def show
+  @article=Article.find(params[:id])
+  end
   
   private
   def article_params
