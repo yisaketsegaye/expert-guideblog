f8612bfd5b6b77b4882775bc3c32dd25ef655083
Yisake <yisaketsega@gmail.com>
2018-08-09 22:15:14 -0400

    Edit and list

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,8 @@
 class ArticlesController < ApplicationController
+  
+  def index
+  @articles=Article.all 
+  end
 def new 
   @article=Article.new
 end 
@@ -13,6 +17,20 @@ end
     end
   end 
   
+  
+  def edit 
+  @article=Article.find(params[:id])
+  end
+
+ def update 
+ @article=Article.find(params[:id])
+   if @article.update(article_params)
+     flash[:notice]="updated succesfully"
+     redirect_to article_path(@article)
+   else
+     render 'edit'
+   end
+ end
   
   def show
   @article=Article.find(params[:id])
