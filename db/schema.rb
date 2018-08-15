07379f4035f1ee21052edf59fd053f723b55f36b
Yisake <yisaketsega@gmail.com>
2018-08-15 14:55:19 -0400

    password user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_14_183607) do
+ActiveRecord::Schema.define(version: 2018_08_15_184112) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2018_08_14_183607) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
