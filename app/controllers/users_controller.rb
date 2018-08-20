93bfd71dc5c434441638777962f760ca8e0629fa
Yisake <yisaketsega@gmail.com>
2018-08-20 14:17:35 -0400

    authntication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,8 +9,9 @@ class UsersController < ApplicationController
   def create
   @user=User.new(user_params)
     if @user.save
-      flash[:success]="welcome #{@user.username}"
-    redirect_to articles_path
+    session[:user_id] =@user.id
+    flash[:success]="welcome #{@user.username}"
+    redirect_to user_path(@user)
     else
       render 'new'
     end
