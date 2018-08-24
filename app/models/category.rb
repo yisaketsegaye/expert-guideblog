78734c9e898a8ba9ed854a2e882d4eae961c725b
Yisake <yisaketsega@gmail.com>
2018-08-24 12:38:31 -0400

    article and association

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,6 @@
 class Category < ApplicationRecord
-  
+  has_many :article_categories 
+  has_many :articles , through: :article_categories
  validates :name  ,presence: true ,  length: {maximum: 25, minimum: 3}
   validates_uniqueness_of :name 
 end
