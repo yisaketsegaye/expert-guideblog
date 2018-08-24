a0ecbe6fd4f8cae3c66fe07249f1da13d7513d32
Yisake <yisaketsega@gmail.com>
2018-08-24 16:43:08 -0400

    edit category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -28,6 +28,27 @@ class CategoriesController < ApplicationController
   end 
   
   
+  
+  def edit 
+    @category=Category.find(params[:id])
+    
+    
+  end 
+  
+  def update
+    @category=Category.find(params[:id])
+    if @category.update(category_params) 
+      flash[:success] = "category name was sucessfuly created "
+      redirect_to category_path(@category)
+      
+    else
+      render 'edit'
+    end 
+    
+  end 
+  
+  
+  
   private
   
   def category_params
